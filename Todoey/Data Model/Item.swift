71d7a949d2dd9ca1ab76e047790cf404ff721abc
tsuyoshi furukawa <vits1339@gmail.com>
2019-07-04 11:28:37 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 古川剛 on 2019/07/04.
+//  Copyright © 2019 古川t剛. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
