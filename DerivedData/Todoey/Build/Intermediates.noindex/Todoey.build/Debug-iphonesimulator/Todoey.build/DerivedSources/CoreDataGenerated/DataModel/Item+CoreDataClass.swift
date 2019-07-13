22d3c40478216c77bcd8e733749cb2de23b68a4e
tsuyoshi furukawa <vits1339@gmail.com>
2019-07-13 13:19:33 +0900

    Persisting fata using Core Data

---
+++ DerivedData/Todoey/Build/Intermediates.noindex/Todoey.build/Debug-iphonesimulator/Todoey.build/DerivedSources/CoreDataGenerated/DataModel/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  
+//
+//  Created by 古川剛 on 2019/07/13.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
